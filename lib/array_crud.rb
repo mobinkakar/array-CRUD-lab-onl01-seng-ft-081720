3443eb70d3caefcb288b99fe0b4d030938433549
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-08-24 16:13:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,34 @@ def create_an_empty_array
 
 end
 
-def create_an_array["Mobin","MOMO","Asim","bummmy"]
+def create_an_array
+  array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
- add_element_to_end_of_array << "Nasiba"
+ array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-add_element_to_start_of_array.unshift("dog")
+array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-remove_element_from_end_of_array.pop("dog")
+array.pop("dog")
 end
 
 def remove_element_from_start_of_array(array)
-remove_element_from_start_of_array("Mobin")
+array.shift("Mobin")
 end
 
 def retrieve_element_from_index(array, index_number)
-retrieve_element_from_index[0]
+array[0]
 end
 
 def retrieve_first_element_from_array(array)
-retrieve_first_element_from_array[0]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-retrieve_last_element_from_array[3]
+array[3]
 end
